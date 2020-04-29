c847692de400fe2ee8d7ab609815f7d3daba5a14
JustynaBroniszewska <jusbroniszewska@gmail.com>
2020-04-29 19:59:50 +0200

    Add basic websocket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const App = () => (
-  <div>
-    Hello React!
-  </div>
-);
+const App = () => {
+  const webSocket = new WebSocket('ws://localhost:3030');
+  const [state, setState] = useState('');
+  useEffect(() => {
+    webSocket.onopen = () => {
+      webSocket.send(JSON.stringify({hej: 'hejeczka'}));};
+    webSocket.onmessage = (event) => {
+      const message = event.data;
+      setState(message);
+    }
+  });
+  
+  return (
+    <div>
+      Hello React!
+      This is server message: {state}
+    </div>
+  )
+};
 
 export default App;
